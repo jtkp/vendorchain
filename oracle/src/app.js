8ba3d9f4810d6a1124ed9e8c82a276fba13b19f1
Daigo <daigo.cd@gmail.com>
2021-07-26 21:57:31 +1000

    Oracle listens and minimally verifies input source

--- oracle/src/app.js
+++ oracle/src/app.js
@@ -1,8 +1,40 @@
-const express = require("express");
+const { response } = require('express')
 
-const app = express();
+// set up express app
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+const isTrustableSource = () => {
+    return true
+}
+
+const sendOnChain = () => {
+    
+}
+
+// Sends updates to blockchain
+// Verifies it by checking permitted sources
+app.post("/updates", (request,response) => {
+    console.log("Received POST /update ");
+    data = request.body;
+    source_ip = request.ip;
+
+    if (!isTrustableSource(source_ip)){
+        return response.send("Untrusable source");
+    }
+
+    // The source is trustable
+    sendOnChain();
+
+    // The data was sent. Successful
+
+    response.send("Update POST");
+});
 
 app.get('/', (req, res) => res.send('HELLO WORLD!'));
 
 const port = 8080;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, '0.0.0.0');
