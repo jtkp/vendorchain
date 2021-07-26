b825899be9300d06c0845446b3cc6d3a6f5883a6
katrinading <xiaoding1998@hotmail.com>
2021-07-26 18:34:01 +0800

    update backend

--- backend/queries.js
+++ backend/queries.js
@@ -3,16 +3,16 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'postgres',
+  database: 'vc2',
   password: 'password',
   port: 5432,
 })
 
 /* ================================ Users ================================*/
 
-// get all users - Katrina
+// get all users - Katrina - checked
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY "userID" ASC', (error, results) => {
+  pool.query('SELECT * FROM userinfo ORDER BY "userID" ASC', (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
@@ -21,11 +21,10 @@ const getUsers = (request, response) => {
   })
 }
 
-// get user by email - Katrina
+// get user by email - Katrina - checked
 const getUserByEmail = (request, response) => {
   const { email } = request.body;
-
-  pool.query(`SELECT * FROM users WHERE "email" = ${email}`, (error, results) => {
+  pool.query('SELECT * FROM userinfo WHERE email = $1', [email], (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
@@ -34,12 +33,12 @@ const getUserByEmail = (request, response) => {
   })
 }
 
-// get a specif user by userId- Katrina
+// get a specif user by userId- Katrina - checked
 const getUserById = (request, response) => {
   // get parameters from url
   const id = request.params.id;
 
-  pool.query(`SELECT * FROM users WHERE "userID" = ${id}`, (error, results) => {
+  pool.query('SELECT * FROM userinfo WHERE "userID" = $1', [id], (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
@@ -48,24 +47,29 @@ const getUserById = (request, response) => {
   })
 }
 
-// add a user - Katrina
+// add a user - Katrina - checked
 const createUser = (request, response) => {
   const { name, email, password } = request.body
 
-  pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',
-              ['test', 'test@test.com', '12345678'],
+  pool.query(`INSERT INTO userinfo (name, email, password) VALUES ('${name}', '${email}', '${password}') returning *`,
               (error, results) => {
     if (error) {
-      response.status(400).json(error);
+      if (error.constraint === 'userinfo_email_key') {
+        response.status(400).json({ "error": "Email existed, please use another email." });
+      } else {
+        response.status(400).json(error);
+      }
+    
+    // if success, get and return user's id
     } else {
-      response.status(200).send({ userId: result.userId })
+      response.status(200).json({ userID: results.rows[0].userID});
     }
   })
 }
 
 /* ================================ Contracts ================================*/
 
-// get all contracts - Katrina
+// get all contracts - Katrina - checked
 const getContracts = (request, response) => {
   pool.query('SELECT * FROM contract ORDER BY "contractID" ASC', (error, results) => {
     if (error) {
@@ -76,14 +80,12 @@ const getContracts = (request, response) => {
   })
 }
 
-// get contract by a specific contract id - Katrina
+// get contract by a specific contract id - Katrina - checked
 const getContractById = (request, response) => {
   // get parameters from url
   const id = request.params.id;
 
-  pool.query(`SELECT * FROM contract WHERE "contractID" = ${id}`,
-              (error, results) => {
-
+  pool.query('SELECT * FROM contract WHERE "contractID" = $1', [id], (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
@@ -92,13 +94,12 @@ const getContractById = (request, response) => {
   })
 }
 
-// get all contracts created by a specific user - Katrina
+// get all contracts created by a specific user - Katrina - checked
 const getContractsByUserId = (request, response) => {
   // get parameters from url
   const id = request.params.userId;
 
-  pool.query(`SELECT * FROM contract WHERE "owner" = ${id}`,
-              (error, results) => {
+  pool.query('SELECT * FROM contract WHERE "owner" = $1', [id], (error, results) => {
 
     if (error) {
       response.status(400).json(error);
@@ -108,15 +109,12 @@ const getContractsByUserId = (request, response) => {
   })
 }
 
-//  get the parties from contract with a specific id - Katrina
+//  get the parties from contract with a specific id - Katrina - to update parites' status
 const getParties  = (request, response) => {
-  const id = request.params.id;
+  const contractId = request.params.id;
 
-  pool.query(`SELECT u.name, u.email
-              FROM party p
-              INNER JOIN contract c on 'c.contractID' = 'p.contractID'
-              INNER JOIN userinfo u on 'partyID' = 'userID'
-              WHERE 'c.contractID' = ${id}`,
+  pool.query('SELECT u.name as parties, u.email as partyEmail, uu.name as invitor FROM party p INNER JOIN contract c on c."contractID" = p."contractID" INNER JOIN userinfo u on "partyID" = "userID" INNER JOIN userinfo uu on "owner" = uu."userID" WHERE c."contractID" = $1',
+              [contractId],
               (error, results) => {
     
     if (error) {
@@ -131,7 +129,7 @@ const getParties  = (request, response) => {
 const deleteContractById = (request, response) => {
   const id = request.params.id;
 
-  pool.query(`DELETE FROM contract where "contractID = ${id}`,
+  pool.query(`DELETE FROM contract where "contractID" = '${id}'`,
             (error, results) => {
 
     if (error) {
@@ -142,10 +140,10 @@ const deleteContractById = (request, response) => {
   })
 }
 
+// invite parties to a contract - Katrina - checked
 const inviteParties = (request, response) => {
   const { contractId, partiesId } = request.body;
-
-  let query = "INSERT INTO parties (partyID, contractID) VALUES";
+  let query = 'INSERT INTO party ("partyID", "contractID") VALUES';
 
   partiesId.map((p) => {
     query += ` ('${p}','${contractId}'),`;
@@ -153,73 +151,77 @@ const inviteParties = (request, response) => {
 
   query = query.slice(0, -1);
   query += ';';
-  console.log(query);
-
   pool.query(query, (error, results) => {
-
     if (error) {
       response.status(400).json(error);
     } else {
-      response.status(200).send(`Successfully invite parties ${partiesId}`);
+      response.status(200).send({ parties: partiesId });
     }
   })
 }
 
-// TODO: create a contract - Katrina
+// create a contract - Katrina - checked
 const createContract = (request, response) => {
   const { title, description, userId  } = request.body
 
-  pool.query('INSERT INTO contract (title, description, state, owner) VALUES ($1, $2, "Not Saved", $3)',
+  pool.query("INSERT INTO contract (title, description, state, owner) VALUES ($1, $2, 'Not Saved', $3) returning *",
               [title, description, userId], 
               (error, results) => {
 
     if (error) {
       response.status(400).json(error);
     } else {
-      response.status(200).send({ contractId: result.contractId })
+      response.status(200).send({ contractID: results.rows[0].contractID })
+
     }
   })
 }
 
-// TODO: update all contracts - Sang
+// update  contracts with contract id - Sang - checked
 const updateContract = (request, response) => {
-  const contractId = parseInt(request.params.contractId);
+  const contractId = request.params.id;
   const { newTitle, newDescription, newAddress } = request.body;
 
   // Use UPDATE keyword
-  pool.query('UPDATE contract SET title = newTitle, description = newDescription, address = newAddress WHERE "contractID" = ${contractId}', (error, results) => {
+  pool.query('UPDATE contract SET title = $1, description = $2, address = $3 WHERE "contractID" = $4 returning * ', 
+              [newTitle, newDescription, newAddress, contractId],
+              (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
-      response.status(200).send(`Contract updated with ID: ${contractId}`);
+      response.status(200).send(results.rows);
     }
   })
 
 }
 
 
-// TODO: update state of a contract - Sang
+//  update state of a contract - Sang - checked
 const updateContractState = (request, response) => {
-  const contractId = parseInt(request.params.contractId);
+  const contractId = request.params.id;
   const { newState } = request.body;
 
   // Use UPDATE keyword
-  pool.query('UPDATE contract SET state = newState WHERE "contractID" = ${contractId}', (error, results) => {
+  pool.query('UPDATE contract SET state = $1 WHERE "contractID" = $2 returning *', 
+              [newState, contractId],
+              (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
-      response.status(200).send(`Contract ${contractId} has updated to state ${newState}`);
+      response.status(200).send(results.rows);
     }
   })
 }
 
 /* ================================ Conditions ================================*/
 
-// TODO: get all conditions with a contract id - Sang
+//  get all conditions with a contract id - Sang
 const getConditions = (request, response) => {
-  const contractId = parseInt(request.params.contractId);
+  const contractId = request.params.contractId;
 
-  pool.query('SELECT * FROM condition WHERE "contractID" = ${contractId} ORDER BY "contractID" ASC', (error, results) => {
+  pool.query('SELECT * FROM condition WHERE "contractID" = $1 ORDER BY "contractID" ASC', 
+              [contractId],
+              (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
@@ -230,13 +232,15 @@ const getConditions = (request, response) => {
 
 // TODO: get a specific condition by an condition id - Sang
 const getConditionById = (request, response) => {
-  const conditionId = parseInt(request.params.id);
+  const conditionId = request.params.id;
 
-  pool.query('SELECT * FROM condition WHERE "conditionID" = ${conditionId} ORDER BY "conditionID" ASC', (error, results) => {
+  pool.query('SELECT * FROM condition WHERE "conditionID" = $1 ORDER BY "conditionID" ASC', 
+              [conditionId],
+              (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
-      response.status(200).json(results.rows[0]);
+      response.status(200).json(results.rows);
     }
   })
 
@@ -244,15 +248,15 @@ const getConditionById = (request, response) => {
 
 // TODO: add a condition into database - Sang
 const addCondition = (request, response) => {
-    const { newDescription, newCategory, newOperator, newValue } = request.body
-  // get data from request.body
+  const { newDescription, newCategory, newOperator, newValue, contractId } = request.body;
 
-  pool.query('INSERT INTO condition (description, category, operator, value) VALUES (newDescription, newCategory, newOperator, newValue)',
+  pool.query('INSERT INTO condition (description, category, operator, value, "contractID") VALUES ($1, $2, $3, $4, $5) returning *',
+              [newDescription, newCategory, newOperator, newValue, contractId],
               (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
-      response.status(200).send('New condition has been added');
+      response.status(200).send(results.rows);
     }
   })
 
@@ -260,14 +264,16 @@ const addCondition = (request, response) => {
 
 // TODO: udpate a specific condition - Sang
 const updateConditionById = (request, response) => {
-  const conditionId = parseInt(request.params.conditionId);
+  const conditionId = request.params.id;
   const { newDescription, newCategory, newOperator, newValue } = request.body;
 
-  pool.query('UPDATE contract SET description = newDescription, category = newCategory, operator = newOperator, value = newValue WHERE "conditionID" = ${conditionId}', (error, results) => {
+  pool.query('UPDATE contract SET description = $1, category = $2, operator = $3, value = $4 WHERE "conditionID" = $5 returning *', 
+            [newDescription, newCategory, newOperator, newValue, conditionId],
+            (error, results) => {
     if (error) {
       response.status(400).json(error);
     } else {
-      response.status(200).send(`Condition has been updated with ID: ${conditionId}`);
+      response.status(200).send(results.rows);
     }
   })
 
@@ -283,7 +289,7 @@ const deleteConditionById = (request, response) => {
     if (error) {
       response.status(400).json(error);
     } else {
-      response.status(200).send(`Condition ${id} has been deleted`);
+      response.status(200).send({ Success: `Condition ${id} has been deleted` });
     }
   })
 }
