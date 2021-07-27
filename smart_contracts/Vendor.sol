2298374b8841629992ec9dd05079ee73eed94485
Justin Pham <z5075823@unsw.edu.au>
2021-07-27 18:40:30 +1000

    added smart contracts

--- smart_contracts/Vendor.sol
+++ smart_contracts/Vendor.sol
@@ -1,21 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
-// npm install @openzeppelin/contracts
-import "@openzeppelin/contracts/access/Ownable.sol";
+// todo: handle contract destruction
 
 /// @title Contract that handles automated payment, considering if conditions are satisfied
-contract Vendor is Ownable {
-    address private manager;
-    address payable private payee;
-    uint private createdOn;
-    uint private expiredOn;
-    uint private nextBillingDate;
-    uint private contractHash;      // need to check variable definition, should be hash of contract terms
-    uint private amount;
+contract Vendor {
+    enum Stages {
+        Initialising,
+        Inactive,
+        Active
+    }
+    Stages public stage = Stages.Initialising;
 
-    bool private satisfied = false;
+    address public manager;
+    address payable public client;
+    address payable public payee;
+
+    string public title;
+    string public description;
+    uint public creationDate;
+    uint public expiryDate;
+    uint public amount;
+    uint public prevBillingDate;
+    uint public nextBillingDate;
+    uint public contractHash;      // need to check variable definition, should be hash of contract terms
+
+    bool public satisfied = false;
     
     // Allows custom conditions and length,
     // but will require multiple small transactions to populate list of conditions
@@ -24,58 +35,128 @@ contract Vendor is Ownable {
         int mode;
         int value;
     }
-
-    Conditions[] conditionArray;
+    Conditions[] public conditionArray;
+    mapping (string => int) private operators;
     
-    // checking if the next billing date has been passed
-    uint constant blocksDaily = 6480;             // Average blocks mined per day
-    uint blockHour = blocksDaily/24;              // Average blocks per hour
-    uint blockMinute = blockHour/60;              // Average blocks per minute
-    uint blockSecond = blockMinute/60;            // Average blocks per second
+    // block calculation
+    uint constant blocksDaily = 6400;                       // Average blocks mined per day
+    uint blocksSecond = blocksDaily/24/60/60;               // Average blocks per second
 
     // values that conditions will check to see if it is satisfied
-    int[] private cumulative;   // sums all values received during the contract
-    int private count;          // keeps track of how many times values were received
+    mapping (uint => int) private cumulative;       // sums all values received during the contract, same length as conditionArray
+    int private count;                              // keeps track of how many times values were received
 
-    // event 
-    
+    // contract is satisfied/unsatisfied at end of valid period
+    event State(address indexed sender, bool isSatisfied);
 
-    // creation is done with factory, so no constructor needed
+    ////////////////////////////////////////////
+    ////////////// INITIALISATION //////////////
+    ////////////////////////////////////////////
 
-    function init(address _manager, address _payee, uint _createdOn, uint _expiredOn, uint _nextBillingDate, uint _contractHash, uint _amount) public {
+    constructor(string memory _title, string memory _description, address _manager, address _client, address _payee, uint _createdOn, uint _expiredOn, uint _prevBillingDate, uint _nextBillingDate, uint _contractHash, uint _amount) {
+        title = _title;
+        description = _description;
         manager = _manager;
+        client = payable(_client);
         payee = payable(_payee);
-        createdOn = _createdOn;
-        expiredOn = _expiredOn;
-        setNextBilling(_nextBillingDate);
+        creationDate = _createdOn;
+        expiryDate = _expiredOn;
+        prevBillingDate = calcDate(_prevBillingDate);
+        nextBillingDate = calcDate(_nextBillingDate);
         contractHash = _contractHash;
         amount = _amount;
+
+        // init operators
+        // >=, <=, ==, <, >
     }
-    
-    /// from the input timestamp, calculate the block after which data will not be accepted
-    function setNextBilling(uint _nextBillingDate) public onlyOwner() {
-        uint blockInterval = (_nextBillingDate - block.timestamp)/blockSecond; // timestamp difference in seconds/blocks per second = blocks from now
-        nextBillingDate = block.number + blockInterval;
+
+    // calculate the block number equivalent to a timestamp
+    function calcDate(uint _billingDate) private view returns (uint) {
+        uint blockInterval = (_billingDate - block.timestamp)/blocksSecond; // timestamp difference in seconds/blocks per second = blocks from now
+        return block.number + blockInterval;
     }
-    
-    function setConds(string[8] memory _names, int[8] memory _values, int[8] memory _modes) public {
+
+    // todo: set so only VendorFactory can call this
+    function setConds(string[8] memory _names, int[8] memory _values, int[8] memory _modes) external atStage(Stages.Initialising) {
         for (uint i = 0; i < 8; i++) {
+            if (keccak256(bytes(_names[i])) == keccak256(bytes("None"))) {
+                endInitStage();
+                return;     // break out of loop when received condition name equals "None"
+            }
             conditionArray.push(Conditions(_names[i], _values[i], _modes[i]));
         }
     }
+
+    function endInitStage() public atStage(Stages.Initialising) {
+        stage = Stages.Inactive;
+
+        // initialise the service data mapping
+        for (uint i = 0; i < conditionArray.length; i++) {
+            cumulative[i] = 0;
+        }
+    }
+
+    function setActive() external atStage(Stages.Inactive) {
+        stage = Stages.Active;
+    }
+
+    function setInactive() external atStage(Stages.Active) {
+        stage = Stages.Inactive;
+    }
     
-    /// mode has to be valid
-    function isSatisfied() private {
+    ////////////////////////////////////////////
+    ////////// DEPLOYED FUNCTIONALITY //////////
+    ////////////////////////////////////////////
+
+    function payContract() external payable atStage(Stages.Active) checkBillingDate() {
+        if (msg.value != amount) {
+            revert();
+        }
+    }
+
+    function balanceOf() external view atStage(Stages.Active) returns (uint) {
+        return address(this).balance;
+    }
+    
+    // todo: what happens when there is no stored balance in the contract???
+    function payVendor() private {
+        payee.transfer(amount);
+        // todo: automated billing date adjustment here??
+    }
+    
+    function refund() private {
+        client.transfer(amount);
+        // todo: automated billing date adjustment here??
+    }
+
+    // Allows off-chain to push data onto the blockchain.
+    // Sums up data received and counts how much of each.
+    // Assumes data for each condition are received at the same time.
+    function receiveServiceData(int[] memory _cumulative) external atStage(Stages.Active) checkBillingDate() {
+        for (uint i = 0; i < _cumulative.length; i++) {
+            cumulative[i] += _cumulative[i];
+        }
+        count++;
+    }
+    
+    // Calculates if the contract terms have been satisfied after next billing date has been passed.
+    function isSatisfied() private atStage(Stages.Active) {
         for (uint i = 0; i < conditionArray.length; i++) {
             int mode = conditionArray[i].mode;
             int value = conditionArray[i].value;
-            int _value = cumulative[i];
+            int _value = cumulative[i]/count;
             
             if (!condValid(mode, _value, value)) {
+                refund();
+                emit State(msg.sender, satisfied);
                 return;
             }
         }
         satisfied = true;
+        emit State(msg.sender, satisfied);
+        
+        // Automatically attempts to make payment.
+        payVendor();
     }
     
     // mode is checked against binary 111,
@@ -100,21 +181,26 @@ contract Vendor is Ownable {
         }
         return true;
     }
+    
+    ////////////////////////////////////////////
+    //////////////// MODIFIERS /////////////////
+    ////////////////////////////////////////////
 
-    // allows off-chain to push data onto the blockchain
-    // immutable, transactions can be tracked
-    function sendServiceData(int[] memory _cumulative) public checkBillingDate() {
-        for (uint i = 0; i < _cumulative.length; i++) {
-            cumulative[i] = _cumulative[i];
-        }
-        count++;
-    }
-
-    // only allow data to be pushed before the billing date
+    // Only allow data to be pushed within the period.
     modifier checkBillingDate() {
-        if (block.number < nextBillingDate) {
+        // Already checked if contract was active before this function,
+        // do not allow new data to be pushed if period is passed.
+        if (block.number > nextBillingDate) {
+            isSatisfied();
+        } else {
+            require(block.number >= prevBillingDate && block.number <= nextBillingDate, "Not within the billing period");
             _;
         }
-        isSatisfied();
+    }
+
+    // Has to be in the correct state.
+    modifier atStage(Stages _stage) {
+        require(stage == _stage, "Contract is not in the required state");
+        _;
     }
 }
--- smart_contracts/VendorFactory.sol
+++ smart_contracts/VendorFactory.sol
@@ -2,12 +2,29 @@
 
 pragma solidity ^0.8.0;
 
-// npm install @openzeppelin/contracts
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./CloneFactory.sol";
 import "./Vendor.sol";
 
+// events
+
 /// @title Factory contract that creates new contracts with different conditions
-contract VendorFactory is CloneFactory, Ownable {
-    Vendor[] private vendors;
+contract VendorFactory {
+    Vendor[] public vendors;
+    // mapping (address => uint) index;
+
+    function createContract(address _manager, address _payee, uint _createdOn, uint _expiredOn, 
+                            uint _nextBillingDate, uint _contractHash, uint _amount, int _contractID) external returns (int) {
+        Vendor newVendor = new Vendor(_manager, _payee, _createdOn, _expiredOn, _nextBillingDate, _contractHash, _amount);
+
+        // no valid contractID index is supplied, therefore it is a new vendor contract
+        if (_contractID == -1) {
+            vendors.push(newVendor);
+            return (vendors.length - 1);
+        }
+
+        // update an old contract
+        // todo: destroy old contract here
+        vendors[_contractID] = newVendor;
+
+        return _contractID;
+    }
 }
